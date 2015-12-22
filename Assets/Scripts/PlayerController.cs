206bd034245b5d604c384c7c3b883099055601bf
Mojopon <tyamuro@gmail.com>
2015-12-22 10:46:07 +0900

    add Move method
    add Move method

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    Vector3 velocity;
+    Rigidbody2D myRigidbody;
+	
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+	public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + (Vector2)velocity * Time.fixedDeltaTime);
+    }
+}
