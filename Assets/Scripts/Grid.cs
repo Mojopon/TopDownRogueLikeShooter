b5b16cd2663c5d0436b556d27c1b2de5c42ebf15
Mojopon <tyamuro@gmail.com>
2015-12-25 09:26:20 +0900

    add grid and node
    add grid and node

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    public MapGenerator mapGenerator;
+
+    public LayerMask unwalkableMask;
+    public float nodeRadius;
+    Node[,] grid;
+
+    void OnDrawGizmos()
+    {
+        if (mapGenerator == null) return;
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(mapGenerator.mapSize.x, mapGenerator.mapSize.y, 1));
+    }
+}
