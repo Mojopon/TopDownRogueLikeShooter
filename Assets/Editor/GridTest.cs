702b148fa13fddff096fadb8cfd242ca4233fed2
Mojopon <tyamuro@gmail.com>
2015-12-27 11:14:02 +0900

    create test for pathfinding
    create test for pathfinding

--- Assets/Editor/GridTest.cs
+++ Assets/Editor/GridTest.cs
@@ -6,11 +6,17 @@ using NSubstitute;
 [TestFixture]
 public class GridTest
 {
+    Node[,] nodes;
     GridImpl grid;
+    int gridSizeX, gridSizeY;
     [SetUp]
     public void SetUp()
     {
-        grid = new GridImpl(10, 10);
+        gridSizeX = 10;
+        gridSizeY = 5;
+
+        nodes = new Node[gridSizeX, gridSizeY];
+        grid = new GridImpl(gridSizeX, gridSizeY);
     }
 
     [Test]
@@ -19,5 +25,50 @@ public class GridTest
         Node node = new Node(true, Vector3.zero, 3, 3);
         grid[0, 0] = node;
         Assert.AreEqual(node, grid[0, 0]);
+    }
+
+    [Test]
+    public void ShouldGetNeighboursOfTargettedNode()
+    {
+        SetupGrid();
+        int targetX, targetY;
+        targetX = 1;
+        targetY = 1;
+        var target = grid[targetX, targetY];
+
+        var neighbours = grid.GetNeightbours(target);
+        for (int y = -1; y < 1; y++)
+        {
+            for (int x = -1; x < 1; x++)
+            {
+                if (x == 0 && y == 0) continue;
+                Assert.IsTrue(neighbours.Contains(nodes[targetX + x, targetY + y]));
+            }
+        }
+
+        neighbours = grid.GetNeightbours(nodes[0, 0]);
+        Assert.IsTrue(neighbours.Contains(nodes[0, 1]));
+        Assert.IsTrue(neighbours.Contains(nodes[1, 1]));
+        Assert.IsTrue(neighbours.Contains(nodes[1, 0]));
+        Assert.IsFalse(neighbours.Contains(nodes[0, 0]));
+
+    }
+
+    [Test]
+    public void ShouldReturnNumberOfNodes()
+    {
+        Assert.AreEqual(gridSizeX * gridSizeY, grid.MaxSize);
+    }
+
+    public void SetupGrid()
+    {
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for(int x = 0; x < gridSizeX; x++)
+            {
+                nodes[x, y] = new Node(true, Vector3.zero, x, y);
+                grid[x, y] = nodes[x, y];
+            }
+        }
     }
 }
--- Assets/Editor/PathfindTest.cs
+++ Assets/Editor/PathfindTest.cs
@@ -6,9 +6,10 @@ using NSubstitute;
 [TestFixture]
 public class PathfindTest
 {
-    private Node[,] nodes;
+    GridImpl grid;
+    Node[,] nodes;
 
-    private int[,] map = new int[,]
+    int[,] map = new int[,]
     {
         { 0,1,0,0,0 },
         { 0,1,0,1,0 },
@@ -17,29 +18,142 @@ public class PathfindTest
         { 0,0,0,1,0 },
     };
 
-    [SetUp]
-    public void InitializeNodes()
+    int[,] map2 = new int[,]
     {
-        int mapSizeX = map.GetLength(0);
-        int mapSizeY = map.GetLength(1);
+        { 0,0,0,0,0,0 },
+        { 1,1,1,1,1,0 },
+        { 0,0,0,0,0,0 },
+        { 0,1,1,1,1,1 },
+        { 0,0,0,0,0,0 },
+    };
+
+    int[,] map3 = new int[,]
+    {
+        { 0,1,0,0,0 },
+        { 0,1,0,1,0 },
+        { 0,1,0,1,0 },
+        { 0,1,0,1,1 },
+        { 0,0,0,1,0 },
+    };
+
+    void SetupTestFixture(int[,] map)
+    {
+        int mapSizeX = map.GetLength(1);
+        int mapSizeY = map.GetLength(0);
+
         nodes = new Node[mapSizeX, mapSizeY];
         for (int y = 0; y < mapSizeY; y++)
         {
             for (int x = 0; x < mapSizeX; x++)
             {
-                CreateNode(x, y, map[x, y] == 0);
+                CreateNode(x, y, map[y, x] == 0);
             }
         }
-    }
 
-    [Test]
-    public void ShouldFindPath()
-    {
+        grid = new GridImpl(mapSizeX, mapSizeY);
 
+        for (int y = 0; y < mapSizeY; y++)
+        {
+            for (int x = 0; x < mapSizeX; x++)
+            {
+                grid[x, y] = nodes[x, y];
+            }
+        }
     }
 
     void CreateNode(int x, int y, bool walkable)
     {
         nodes[x, y] = new Node(walkable, Vector3.zero, x, y);
+    }
+
+    [Test]
+    public void ShouldFindPath()
+    {
+        SetupTestFixture(map);
+
+        var pathfinder = new PathfindingImpl(grid);
+
+        var startNode = nodes[0, 0];
+        var targetNode = nodes[4, 4];
+
+        var path = pathfinder.FindPath(startNode, targetNode);
+        path.Reverse();
+
+        Assert.AreEqual(12, path.Count);
+
+        Assert.AreEqual(nodes[0, 1], path[0]);
+        Assert.AreEqual(nodes[0, 2], path[1]);
+        Assert.AreEqual(nodes[0, 3], path[2]);
+        Assert.AreEqual(nodes[1, 4], path[3]);
+        Assert.AreEqual(nodes[2, 3], path[4]);
+        Assert.AreEqual(nodes[2, 2], path[5]);
+        Assert.AreEqual(nodes[2, 1], path[6]);
+        Assert.AreEqual(nodes[3, 0], path[7]);
+        Assert.AreEqual(nodes[4, 1], path[8]);
+        Assert.AreEqual(nodes[4, 2], path[9]);
+        Assert.AreEqual(nodes[4, 3], path[10]);
+        Assert.AreEqual(nodes[4, 4], path[11]);
+
+        //{ 0,1,0,0,0 }
+        //{ 0,1,0,1,0 }
+        //{ 0,1,0,1,0 }
+        //{ 0,1,0,1,0 }
+        //{ 0,0,0,1,0 }
+    }
+
+    [Test]
+    public void PathFindTest2()
+    {
+
+        SetupTestFixture(map2);
+
+        var pathfinder = new PathfindingImpl(grid);
+
+        var startNode = nodes[0, 0];
+        var targetNode = nodes[5, 4];
+
+        var path = pathfinder.FindPath(startNode, targetNode);
+        path.Reverse();
+
+        Assert.AreEqual(15, path.Count);
+
+        Assert.AreEqual(nodes[1, 0], path[0]);
+        Assert.AreEqual(nodes[2, 0], path[1]);
+        Assert.AreEqual(nodes[3, 0], path[2]);
+        Assert.AreEqual(nodes[4, 0], path[3]);
+        Assert.AreEqual(nodes[5, 1], path[4]);
+        Assert.AreEqual(nodes[4, 2], path[5]);
+        Assert.AreEqual(nodes[3, 2], path[6]);
+        Assert.AreEqual(nodes[2, 2], path[7]);
+        Assert.AreEqual(nodes[1, 2], path[8]);
+        Assert.AreEqual(nodes[0, 3], path[9]);
+        Assert.AreEqual(nodes[1, 4], path[10]);
+        Assert.AreEqual(nodes[2, 4], path[11]);
+        Assert.AreEqual(nodes[3, 4], path[12]);
+        Assert.AreEqual(nodes[4, 4], path[13]);
+        Assert.AreEqual(nodes[5, 4], path[14]);
+
+
+        //{ 0,0,0,0,0,0 }
+        //{ 1,1,1,1,1,0 }
+        //{ 0,0,0,0,0,0 }
+        //{ 0,1,1,1,1,1 }
+        //{ 0,0,0,0,0,0 }
+
+    }
+
+    [Test]
+    public void ShouldReturnBlankListWhenNoPathAvailable()
+    {
+        SetupTestFixture(map3);
+
+        var pathfinder = new PathfindingImpl(grid);
+
+        var startNode = nodes[0, 0];
+        var targetNode = nodes[4, 4];
+
+        var path = pathfinder.FindPath(startNode, targetNode);
+
+        Assert.AreEqual(0, path.Count);
     }
 }
