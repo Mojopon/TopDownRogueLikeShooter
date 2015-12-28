d20fe6ae2cbe6e403e877413d1c03fbfc1ce16aa
Mojopon <tyamuro@gmail.com>
2015-12-28 10:22:40 +0900

    commit
    commit

---
+++ Assets/Scripts/PathReference.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathReference
+{
+    IGrid grid;
+    public PathReference(IGrid _grid)
+    {
+        grid = _grid;
+    }
+
+
+
+}
